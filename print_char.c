ae9e553a73faa18a8bba79e23d748a834f12cd7e
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-25 15:47:07 +0100

    oublie de c

---
+++ print_char.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_char- print a character using _putchar
+ * @arg: argument list
+ */
+
+void print_char(va_list arg)
+{
+ 	_putchar(va_arg(arg, int));   
+}
---
+++ print_percentage.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_percentage - print a percentage using _putchar
+ * @arg: argument list
+ */
+
+void print_percentage(va_list arg)
+{
+ 	_putchar('\%');   
+}
