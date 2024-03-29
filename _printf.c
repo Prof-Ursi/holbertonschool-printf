d0da12ee8cc85bb0fb927a2158114541897791de
SamJouini <samantha.u.h.jouini@gmail.com>
2024-03-29 09:27:56 +0100

    betty make me do it

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  * _printf - function that print anything
  * @format:list of types of arguments passed to the function
@@ -9,42 +11,38 @@
 int _printf(const char *format, ...)
 {
 	va_list arg_list;
-	int i = 0;
-	int j;
-	int length = 0;
+	int i = 0, j, length = 0;
 
-	type_t format_list[] = {
-		{'c', print_char},
-		{'i', print_decimal},
-		{'d', print_decimal},
-		{'s', print_string},
-		{'%', print_percentage}
-	};
+	type_t format_list[] = {{'c', print_char}, {'i', print_decimal},
+	{'d', print_decimal}, {'s', print_string},	{'%', print_percentage}};
+
 	va_start(arg_list, format);
+
 	while (format != NULL && format[i] != '\0')
 	{
-		j = 0;
-
 		if (format[i] == '%')
 		{
-			while (j < 5)
+			for (j = 0; j < 5; j++)
 			{
 				if (format[i + 1] == format_list[j].type)
 				{
 					length += format_list[j].f(arg_list);
-					i++;
+					i = i + 2;
 					break;
 				}
-				j++;
 			}
+			if (j == 5)
+			{
+				length += _putchar(format[i++]);
+			}
+
 		}
 		else
 		{
-			_putchar(format[i]);
+			length += _putchar(format[i++]);
 		}
-		i++;
-		length++;
 	}
+
 	va_end(arg_list);
 	return (length);
 }
